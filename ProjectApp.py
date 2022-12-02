f1803b5cce669f3d399288f7226c2cd0a8bbc575
fredydevelop <105555015+fredydevelop@users.noreply.github.com>
2022-12-02 21:15:49 +0100

    Update ProjectApp.py

--- ProjectApp.py
+++ ProjectApp.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pickle as pk
 from streamlit_option_menu import option_menu
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
